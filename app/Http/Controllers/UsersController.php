a8b45260a5f045056b826b5235e8f56ec70edf74
lxlpokie <921586186@qq.com>
2019-04-09 08:51:43 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,22 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    public function store(Request $request)
+    {
+    	$this->validate($request,[
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|uniqid::users|max:255',
+    		'password' => 'required|confirmd|min:6',
+    	]);
 
+    	$user = User::create([
+    		'name' => $request->name,
+    		'eamil' => $request->eamil,
+    		'password' => bcrypt($request->password),
+    	]);
+
+    	session()->flash('success','恭喜你注册成功');
+    	return redirect()->route('users.show',[$user]);
+    }
 
 }
