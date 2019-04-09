f8ac3072b2fb819cac27d1cd1f37429aaeb7eaf6
lxlpokie <921586186@qq.com>
2019-04-09 09:31:21 +0800

    登陆成功后导航

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,20 +20,22 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-    	$this->validate($request,[
-    		'name' => 'required|max:50',
-    		'email' => 'required|email|uniqid::users|max:255',
-    		'password' => 'required|confirmd|min:6',
-    	]);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
 
-    	$user = User::create([
-    		'name' => $request->name,
-    		'eamil' => $request->eamil,
-    		'password' => bcrypt($request->password),
-    	]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
 
-    	session()->flash('success','恭喜你注册成功');
-    	return redirect()->route('users.show',[$user]);
+        Auth::login($user);
+
+        session()->flash('success', '恭喜你注册成功');
+        return redirect()->route('users.show', [$user]);
     }
 
 }
